c1f3268bf9576fc193627f3108ba97c8c9dd718b
palaksinghania05 <palak.singhania_cs.ccv19@gla.ac.in>
2021-09-16 00:19:48 +0530

    added september starter 1

---
+++ src/september/FindDirection.java
@@ -0,0 +1,24 @@
+package september;
+
+import java.util.Scanner;
+
+public class FindDirection {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int T = sc.nextInt();
+            while(T>0){
+                int X = sc.nextInt();
+                if(X%4==0)
+                    System.out.println("North");
+                else if(X%4==1)
+                    System.out.println("East");
+                else if(X%4==2)
+                    System.out.println("South");
+                else
+                    System.out.println("West");
+                T--;
+            }
+        }
+    }
+}
