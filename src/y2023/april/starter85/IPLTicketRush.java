48837a3f0e4298d8a930d167a5eb922202fe08fa
Palak Singhania <singhaniapalak2002@gmail.com>
2023-04-13 23:59:01 +0530

    added starters 85 april 2023

---
+++ src/y2023/april/starter85/IPLTicketRush.java
@@ -0,0 +1,15 @@
+package y2023.april.starter85;
+
+import java.util.Scanner;
+
+public class IPLTicketRush {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+
+        }
+    }
+}
