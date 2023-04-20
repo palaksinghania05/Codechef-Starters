372146bf6840863053c9e7624ebc2800695c4634
Palak Singhania <singhaniapalak2002@gmail.com>
2023-04-20 23:50:05 +0530

    added starters 86 april 2023

---
+++ src/y2023/april/starter86/CodechefLearnProblemSolving.java
@@ -0,0 +1,11 @@
+package y2023.april.starter86;
+
+import java.util.Scanner;
+
+public class CodechefLearnProblemSolving {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(2 * N);
+    }
+}
