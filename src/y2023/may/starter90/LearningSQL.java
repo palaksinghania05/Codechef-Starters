4ab3491928881ae41501ffa973115046bd340583
Palak Singhania <singhaniapalak2002@gmail.com>
2023-05-18 21:54:46 +0530

    added starters 90 may 2023

---
+++ src/y2023/may/starter90/LearningSQL.java
@@ -0,0 +1,13 @@
+package y2023.may.starter90;
+
+import java.util.Scanner;
+
+public class LearningSQL {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int R = sc.nextInt();
+        int C = sc.nextInt();
+        int E = sc.nextInt();
+        System.out.println((R + E) * C);
+    }
+}
