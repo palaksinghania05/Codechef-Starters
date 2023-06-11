1eb215c2f02769592f34f111c6284ebf0ef5faf4
Palak Singhania <singhaniapalak2002@gmail.com>
2023-06-11 20:57:39 +0530

    added starters 93 june 2023

--- src/y2023/june/starter93/Problem.java
+++ src/y2023/june/starter93/Problem.java
@@ -1,2 +1,21 @@
-package y2023.june.starter93;public class Problem {
+package y2023.june.starter93;
+
+import java.util.Scanner;
+
+public class Problem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            int X = Math.abs(N - M);
+            if (X % 2 == 0 || X % 4 == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+            T--;
+        }
+    }
 }
--- src/y2023/june/starter93/SevenRings.java
+++ src/y2023/june/starter93/SevenRings.java
@@ -1,2 +1,30 @@
-package y2023.june.starter93;public class SevenRings {
+package y2023.june.starter93;
+
+import java.util.Scanner;
+
+public class SevenRings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int X = sc.nextInt();
+            int D = N * X;
+            int count = 0;
+            int aliasD = D;
+            while (aliasD % 10 >= 0) {
+                count = count + 1;
+                aliasD = aliasD / 10;
+                if (aliasD == 0) {
+                    break;
+                }
+            }
+            if (count == 5) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+            T--;
+        }
+    }
 }
