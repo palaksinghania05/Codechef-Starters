a8f2f053b2b3d3c6f6be179d61a9337efa56bb99
Palak Singhania <singhaniapalak2002@gmail.com>
2023-07-05 22:34:23 +0530

    added starters 97 july 2023

---
+++ src/y2023/july/starter97/TwoThousand.java
@@ -0,0 +1,11 @@
+package y2023.july.starter97;
+
+import java.util.Scanner;
+
+public class TwoThousand {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N * 4);
+    }
+}
