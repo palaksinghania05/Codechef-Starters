26b5a678365d905782ea285d448595b101ded04e
Palak Singhania <singhaniapalak2002@gmail.com>
2023-10-18 23:17:40 +0530

    added starters 104 october 2023

---
+++ src/y2023/october/starter104/DoubleRent.java
@@ -0,0 +1,11 @@
+package y2023.october.starter104;
+
+import java.util.Scanner;
+
+public class DoubleRent {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        System.out.println(2 * X);
+    }
+}
