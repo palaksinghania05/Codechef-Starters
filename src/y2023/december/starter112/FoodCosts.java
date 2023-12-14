2f9ee6903a97c0eca98d4415051ff555af3e8bfa
Palak Singhania <singhaniapalak2002@gmail.com>
2023-12-14 21:55:34 +0530

    added starters 112 december 2023

---
+++ src/y2023/december/starter112/FoodCosts.java
@@ -0,0 +1,12 @@
+package y2023.december.starter112;
+
+import java.util.Scanner;
+
+public class FoodCosts {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        System.out.println(X * 6 + Y);
+    }
+}
