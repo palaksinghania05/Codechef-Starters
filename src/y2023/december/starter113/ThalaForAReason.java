ada3f8b105692bf385b74c065a4ff67c814656fe
Palak Singhania <singhaniapalak2002@gmail.com>
2023-12-21 19:35:33 +0530

    added starters 113 december 2023

---
+++ src/y2023/december/starter113/ThalaForAReason.java
@@ -0,0 +1,15 @@
+package y2023.december.starter113;
+
+import java.util.Scanner;
+
+public class ThalaForAReason {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if (N == 7) {
+            System.out.println("THALA");
+        } else {
+            System.out.println("SADGE");
+        }
+    }
+}
