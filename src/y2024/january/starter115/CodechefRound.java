5de9b04dbf13e81f62f8a9b9414c1d7d62122d55
Palak Singhania <singhaniapalak2002@gmail.com>
2024-01-04 20:41:06 +0530

    added starters 115 january 2024

---
+++ src/y2024/january/starter115/CodechefRound.java
@@ -0,0 +1,15 @@
+package y2024.january.starter115;
+
+import java.util.Scanner;
+
+public class CodechefRound {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if (N == 4) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
