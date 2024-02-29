7e41a535d25a699a74a096e215cb983cc39508ad
Palak Singhania <singhaniapalak2002@gmail.com>
2024-02-29 20:40:49 +0530

    added starters 123 february 2024

---
+++ src/y2024/february/starter123/AlgomaniacFinals.java
@@ -0,0 +1,14 @@
+package y2024.february.starter123;
+
+import java.util.Scanner;
+
+public class AlgomaniacFinals {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        if (X == 17)
+            System.out.println("Yay");
+        else
+            System.out.println("no");
+    }
+}
