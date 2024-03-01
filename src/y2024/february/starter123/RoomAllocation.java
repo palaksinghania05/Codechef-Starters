05b305e0718e68058d72cc5b7dfd67d34e95f1ed
Palak Singhania <singhaniapalak2002@gmail.com>
2024-03-01 21:05:03 +0530

    added starters 123 february 2024

---
+++ src/y2024/february/starter123/RoomAllocation.java
@@ -0,0 +1,21 @@
+package y2024.february.starter123;
+
+import java.util.Scanner;
+
+public class RoomAllocation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int[] A = new int[N];
+            int result = 0;
+            for (int i = 0; i < N; i++) {
+                A[i] = sc.nextInt();
+                result += (A[i] / 2) + (A[i] % 2);
+            }
+            System.out.println(result);
+            T--;
+        }
+    }
+}
