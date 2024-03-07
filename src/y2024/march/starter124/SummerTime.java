ad03e7f8b249cfed8fd9c66c16ba1bef117b858e
Palak Singhania <singhaniapalak2002@gmail.com>
2024-03-07 23:18:31 +0530

    added starters 124 march 2024

---
+++ src/y2024/march/starter124/SummerTime.java
@@ -0,0 +1,15 @@
+package y2024.march.starter124;
+
+import java.util.Scanner;
+
+public class SummerTime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        if (X > 35) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
