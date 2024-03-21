e259c76cffea89cee92b089026e3e8054673c8e2
Palak Singhania <singhaniapalak2002@gmail.com>
2024-03-21 22:43:06 +0530

    added starters 126 march 2024

---
+++ src/y2024/march/starter126/ACPlease.java
@@ -0,0 +1,15 @@
+package y2024.march.starter126;
+
+import java.util.Scanner;
+
+public class ACPlease {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if (N > 30) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
