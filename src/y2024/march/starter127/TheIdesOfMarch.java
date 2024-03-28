63ed8bc318d070a5dc570aa5ff3d13c4c59a1b00
Palak Singhania <singhaniapalak2002@gmail.com>
2024-03-28 21:27:22 +0530

    added starters 127 march 2024

---
+++ src/y2024/march/starter127/TheIdesOfMarch.java
@@ -0,0 +1,15 @@
+package y2024.march.starter127;
+
+import java.util.Scanner;
+
+public class TheIdesOfMarch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if (N == 15) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
