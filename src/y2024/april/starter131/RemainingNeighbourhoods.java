28577cbcbf69d168ddf7927c896a9bd431c0d3f6
Palak Singhania <singhaniapalak2002@gmail.com>
2024-04-25 23:54:44 +0530

    added starters 131 april 2024

---
+++ src/y2024/april/starter131/RemainingNeighbourhoods.java
@@ -0,0 +1,11 @@
+package y2024.april.starter131;
+
+import java.util.Scanner;
+
+public class RemainingNeighbourhoods {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(100 - N);
+    }
+}
