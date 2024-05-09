2ab96c0d4d586c8c5f72eae1a30dc20550261288
Palak Singhania <singhaniapalak2002@gmail.com>
2024-05-09 22:09:41 +0530

    added starters 133 may 2024

---
+++ src/y2024/may/starter133/DevouringDonuts.java
@@ -0,0 +1,12 @@
+package y2024.may.starter133;
+
+import java.util.Scanner;
+
+public class DevouringDonuts {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        System.out.println(X * Y);
+    }
+}
