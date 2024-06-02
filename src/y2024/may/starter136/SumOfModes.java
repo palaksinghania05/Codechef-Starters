c3ed64608a35579fdedb0e49e84d42765a4591df
Palak Singhania <singhaniapalak2002@gmail.com>
2024-06-02 23:53:14 +0530

    added starters 136 may 2024

--- src/y2024/may/starter136/SumOfModes.java
+++ src/y2024/may/starter136/SumOfModes.java
@@ -1,7 +1,6 @@
 package y2024.may.starter136;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class SumOfModes {
@@ -9,37 +8,21 @@ public class SumOfModes {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         while (T > 0) {
-            int N = sc.nextInt();
+            long N = sc.nextLong();
             String S = sc.next();
-            int[] arr = new int[N];
-            long result = (N * (long) (N + 1)) / 2;
+            HashMap<Long, Long> map = new HashMap<>();
+            long sum = 0;
+            long count = 0;
+            map.put(0L, 1L);
             for (int i = 0; i < N; i++) {
-                arr[i] = (S.charAt(i) == '0') ? -1 : 1;
+                sum += (S.charAt(i) == '0') ? 1 : -1;
+                count += map.getOrDefault(sum, 0L);
+                map.put(sum, map.getOrDefault(sum, 0L) + 1);
             }
-            long[] temp = new long[N];
-            temp[0] = arr[0];
-            for (int i = 1; i < N; i++) {
-                temp[i] = arr[i] + temp[i - 1];
-            }
-            Map<Long, Long> map = new HashMap<>();
-            if (N == 1) {
-                System.out.println(1);
-            } else {
-                map.put((long) 0, (long) 1);
-                int i = 0;
-                while (i < N) {
-                    long sum = temp[i];
-                    if (map.containsKey(sum)) {
-                        result = result + map.get(sum);
-                        map.put(sum, map.get(sum) + 1);
-                    } else {
-                        map.put(sum, (long) 1);
-                    }
-                    i++;
-                }
-                System.out.println(result);
-                T--;
-            }
+
+            long result = (N * (N + 1)) / 2;
+            System.out.println(result + count);
+            T--;
         }
     }
 }
