46eedd04e7db06ff3d264a85bd9cf66c145cb8f9
Palak Singhania <singhaniapalak2002@gmail.com>
2024-06-27 23:57:45 +0530

    added starters 140 june 2024

---
+++ src/y2024/june/starter140/YogaDay.java
@@ -0,0 +1,12 @@
+package y2024.june.starter140;
+
+import java.util.Scanner;
+
+public class YogaDay {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int ans = N / 12;
+        System.out.println(ans);
+    }
+}
