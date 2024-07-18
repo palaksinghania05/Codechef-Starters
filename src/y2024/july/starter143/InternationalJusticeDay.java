89e5675142dd41c77265774899b8cad190c05e3f
Palak Singhania <singhaniapalak2002@gmail.com>
2024-07-18 23:58:10 +0530

    added starters 142 july 2024

---
+++ src/y2024/july/starter143/InternationalJusticeDay.java
@@ -0,0 +1,12 @@
+package y2024.july.starter143;
+
+import java.util.Scanner;
+
+public class InternationalJusticeDay {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        System.out.println(X >= Y ? "yes" : "no");
+    }
+}
