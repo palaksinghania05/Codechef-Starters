18a66e297670009d8206f216da2080ed372ea868
Palak Singhania <singhaniapalak2002@gmail.com>
2024-08-22 13:36:45 +0530

    added starters 148 august 2024

---
+++ src/y2024/august/starter148/ClearanceSale.java
@@ -0,0 +1,13 @@
+package y2024.august.starter148;
+
+import java.util.Scanner;
+
+public class ClearanceSale {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int Y = X / 2;
+        int Z = X + Y;
+        System.out.println(Z);
+    }
+}
