b5ece4eebbe6c9aada07a04bab1e6aef0aecf369
Palak Singhania <singhaniapalak2002@gmail.com>
2024-09-05 23:44:43 +0530

    added starters 150 sept 2024

---
+++ src/y2024/september/starter150/IOI2024.java
@@ -0,0 +1,15 @@
+package y2024.september.starter150;
+
+import java.util.Scanner;
+
+public class IOI2024 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        if (X <= 8) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
