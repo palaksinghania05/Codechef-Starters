ae45fc8c3fc0c516ea3923d9dbcbf1b48312a95c
Palak Singhania <singhaniapalak2002@gmail.com>
2024-10-10 23:57:24 +0530

    added starters 155 oct 2024

---
+++ src/y2024/october/starter155/ChefAndParole.java
@@ -0,0 +1,11 @@
+package y2024.october.starter155;
+
+import java.util.Scanner;
+
+public class ChefAndParole {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        System.out.println(X >= 7 ? "Yes" : "No");
+    }
+}
