83fc240cd07eea00d451877c4b2c0dfadb56afa5
Palak Singhania <singhaniapalak2002@gmail.com>
2024-12-15 23:48:20 +0530

    added starters 164 dec 2024

--- src/y2024/december/starter164/HalloweenArray.java
+++ src/y2024/december/starter164/HalloweenArray.java
@@ -6,24 +6,24 @@ import java.util.Scanner;
 public class HalloweenArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T > 0) {
-            int N = sc.nextInt();
-            long L = sc.nextLong();
-            long R = sc.nextLong();
+        int T = sc.nextInt(); // No. of test cases
+        while (T-- > 0) {
+            int N = sc.nextInt(); // No. of elements in the array
+            long L = sc.nextLong(); // Left bound
+            long R = sc.nextLong(); // Right bound
             long[] arr = new long[N];
             for (int i = 0; i < N; i++) {
                 arr[i] = sc.nextLong();
             }
             Arrays.sort(arr);
-            boolean flag = false;
+            boolean flag1 = false;
             for (int i = 0; i < N - 1; i++) {
                 if (arr[i] == arr[i + 1]) {
-                    flag = true;
+                    flag1 = true;
                     break;
                 }
             }
-            if (flag) {
+            if (flag1) {
                 if (L <= 0 && R >= 0) {
                     System.out.println("YES");
                 } else {
@@ -31,25 +31,25 @@ public class HalloweenArray {
                 }
                 continue;
             }
-            long temp = 1;
+            long product = 1;
             boolean flag2 = false;
             outer:
             for (int i = 0; i < N - 1; i++) {
                 for (int j = i + 1; j < N; j++) {
                     long xor = arr[i] ^ arr[j];
-                    temp *= xor;
-                    if (temp > R) {
+                    product *= xor;
+                    if (product > R) {
                         flag2 = true;
                         break outer;
                     }
                 }
             }
-            if (flag2 || temp < L || temp > R) {
+            if (flag2 || product < L || product > R) {
                 System.out.println("NO");
             } else {
                 System.out.println("YES");
             }
-            T--;
         }
+        sc.close();
     }
 }
