6e0c1098116e641870eb2e09abb2adbeebc83ee8
Palak Singhania <singhaniapalak2002@gmail.com>
2025-01-02 19:57:58 +0530

    added starters 167 jan 2025

---
+++ src/y2025/january/starter167/HappyNewYear.java
@@ -0,0 +1,11 @@
+package y2025.january.starter167;
+
+import java.util.Scanner;
+
+public class HappyNewYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        System.out.println(24 - X);
+    }
+}
