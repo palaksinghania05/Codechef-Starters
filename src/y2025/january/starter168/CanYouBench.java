a4b709a0283afe02356fd5dbec4fae1b78870e67
Palak Singhania <singhaniapalak2002@gmail.com>
2025-01-09 23:32:17 +0530

    added starters 168 jan 2025

---
+++ src/y2025/january/starter168/CanYouBench.java
@@ -0,0 +1,11 @@
+package y2025.january.starter168;
+
+import java.util.Scanner;
+
+public class CanYouBench {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        System.out.println(X + 20);
+    }
+}
