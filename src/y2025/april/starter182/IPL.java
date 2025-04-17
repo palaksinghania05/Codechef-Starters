c9a8e3c8b1be8e43e45fa8ab2301bd8a28e88535
Palak Singhania <singhaniapalak2002@gmail.com>
2025-04-17 22:56:22 +0530

    added starters 182 April 2025

---
+++ src/y2025/april/starter182/IPL.java
@@ -0,0 +1,15 @@
+package y2025.april.starter182;
+
+import java.util.Scanner;
+
+public class IPL {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        if (X >= 7) {
+            System.out.println("THALA");
+        } else {
+            System.out.println("BOOM");
+        }
+    }
+}
