469a0fbccf7f17665ef86fd0aede239bd9a87dd6
Palak Singhania <singhaniapalak2002@gmail.com>
2025-04-24 23:05:29 +0530

    added starters 183 April 2025

---
+++ src/y2025/april/starter183/SixersMax.java
@@ -0,0 +1,11 @@
+package y2025.april.starter183;
+
+import java.util.Scanner;
+
+public class SixersMax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        System.out.println(T / 6);
+    }
+}
