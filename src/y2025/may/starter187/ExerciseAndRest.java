20b1aea3886faaa9a504ee5348f99fb789101cf6
Palak Singhania <singhaniapalak2002@gmail.com>
2025-05-22 22:44:51 +0530

    added starters 187 may 2025

---
+++ src/y2025/may/starter187/ExerciseAndRest.java
@@ -0,0 +1,11 @@
+package y2025.may.starter187;
+
+import java.util.Scanner;
+
+public class ExerciseAndRest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N * 3);
+    }
+}
