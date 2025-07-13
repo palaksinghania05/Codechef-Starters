84c9417d8f1918da0cc0eeb21ce324090569dcc9
Palak Singhania <singhaniapalak2002@gmail.com>
2025-07-13 23:37:33 +0530

    added starters 194 july 2025

---
+++ src/y2025/july/starter194/ChefBakesCake1.java
@@ -0,0 +1,12 @@
+package y2025.july.starter194;
+
+import java.util.Scanner;
+
+public class ChefBakesCake1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        System.out.println(M * 50 - N * 30);
+    }
+}
