f2fa89626cd37dae14bdb96ca1492f3b0203327b
Palak Singhania <singhaniapalak2002@gmail.com>
2025-08-28 23:23:23 +0530

    added starters 201 august 2025

---
+++ src/y2025/august/starter201/SellingCoins.java
@@ -0,0 +1,12 @@
+package y2025.august.starter201;
+
+import java.util.Scanner;
+
+public class SellingCoins {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A + B * 2);
+    }
+}
