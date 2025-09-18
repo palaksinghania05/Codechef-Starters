b1ce380e4cdb13eee36e6bb68461963e0968ae79
Palak Singhania <singhaniapalak2002@gmail.com>
2025-09-18 23:06:49 +0530

    added starters 204 sept 2025

---
+++ src/y2025/september/starter204/Triangles.java
@@ -0,0 +1,12 @@
+package y2025.september.starter204;
+
+import java.util.Scanner;
+
+public class Triangles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(180 - A - B);
+    }
+}
