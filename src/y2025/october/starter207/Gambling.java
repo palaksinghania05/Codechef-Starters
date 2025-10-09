48604f0ec4f0ef1a980fbd51df5c701c7de117c2
Palak Singhania <singhaniapalak2002@gmail.com>
2025-10-09 23:28:34 +0530

    added starters 207 oct 2025

---
+++ src/y2025/october/starter207/Gambling.java
@@ -0,0 +1,11 @@
+package y2025.october.starter207;
+
+import java.util.Scanner;
+
+public class Gambling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        System.out.println(7 - A);
+    }
+}
