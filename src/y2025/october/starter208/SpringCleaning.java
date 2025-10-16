3b64a6382355542b6945c5748ec9deb16f88c549
Palak Singhania <singhaniapalak2002@gmail.com>
2025-10-16 22:42:23 +0530

    added starters 208 oct 2025

---
+++ src/y2025/october/starter208/SpringCleaning.java
@@ -0,0 +1,12 @@
+package y2025.october.starter208;
+
+import java.util.Scanner;
+
+public class SpringCleaning {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        System.out.println(X * 30 + Y * 60);
+    }
+}
