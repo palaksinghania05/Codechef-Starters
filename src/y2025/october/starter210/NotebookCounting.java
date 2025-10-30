0c2131b30e40059e6bba1776291d6d380a2aa6ed
Palak Singhania <singhaniapalak2002@gmail.com>
2025-10-30 23:56:21 +0530

    added starters 210 oct 2025

---
+++ src/y2025/october/starter210/NotebookCounting.java
@@ -0,0 +1,12 @@
+package y2025.october.starter210;
+
+import java.util.Scanner;
+
+public class NotebookCounting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A * B * 100);
+    }
+}
