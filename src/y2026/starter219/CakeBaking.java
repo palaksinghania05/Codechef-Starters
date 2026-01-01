2c92e6335d03f85e666a83a6aee8df351cf6d598
Palak Singhania <singhaniapalak2002@gmail.com>
2026-01-01 22:55:56 +0530

    added starters 219 jan 2026

---
+++ src/y2026/starter219/CakeBaking.java
@@ -0,0 +1,12 @@
+package y2026.starter219;
+
+import java.util.Scanner;
+
+public class CakeBaking {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        System.out.println(Math.min(N, M - N));
+    }
+}
