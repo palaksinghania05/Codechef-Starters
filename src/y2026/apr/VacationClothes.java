457a9739b64af998b723e3c9c89056908ef1066a
Palak Singhania <singhaniapalak2002@gmail.com>
2026-04-02 23:56:35 +0530

    added starters 232 apr 2026

---
+++ src/y2026/apr/VacationClothes.java
@@ -0,0 +1,11 @@
+package y2026.apr;
+
+import java.util.Scanner;
+
+public class VacationClothes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(Math.min(N, 7));
+    }
+}
